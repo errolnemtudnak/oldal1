aad5f13f8061e78b5a84dafdd92a213628a332fb
errolnemtudnak <plsdontrage@proton.me>
2026-05-02 21:45:40 +0200

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,14 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-
-/** @type {import('@sveltejs/kit').Config} */
+import adapter from '@sveltejs/adapter-static';
+ 
+/** @type {imdwqdport('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: {
+		adapter: adapter({
+			fallback: '404.html'
+		})
+	},
 };
-
+ 
+config.paths = { base: process.argv.includes('dev') ? '' : "/ngentotstw" }
+ 
 export default config;
